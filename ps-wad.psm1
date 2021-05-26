f9b0bb124cfe8603fab0a1d97ca46eda5607ba4c
batman <batman@thezone.local>
2021-05-25 20:43:11 -0400

    init

---
+++ ps-wad.psm1
@@ -0,0 +1,79 @@
+﻿#this file is for functions that are useful or do basic things that PS should have
+
+#returns true if 'match' operator returns true on all array members.
+#case sensitivity is toggleable
+function Get-MatchAll{
+    param(
+        $Array,
+        $String,
+        [bool]$CaseSensitive = $false
+    )
+
+    $match = $true
+
+    foreach($thing in $array)
+    {
+        if($CaseSensitive)
+        {
+            if($thing -cnotmatch $string){$match = $false}
+        }
+        else
+        {
+            if($thing -notmatch $String){$match = $false}
+        }
+    }
+
+    return $match
+}
+
+#returns true when 'match' operator returns false on all array members
+#case sensitivity is toggleable
+function Get-MatchNone{
+    param(
+        $Array,
+        $String,
+        [bool]$CaseSensitive = $false
+    )
+
+    $match = $true
+
+    foreach($thing in $array)
+    {
+        if($CaseSensitive)
+        {
+            if($thing -cmatch $String){$match = $false}
+        }
+        else
+        {
+            if($thing -match $String){$match = $false}
+        }
+    }
+
+    return $match
+}
+
+#returns true if ANY array members return 'true' from the 'match operator
+#case sensitivity is toggleable
+function Get-MatchAny{
+    param(
+        $Array,
+        $String,
+        [bool]$CaseSensitive = $false
+    )
+
+    $match = $false
+
+    foreach($thing in $Array)
+    {
+        if($CaseSensitive)
+        {
+            if($thing -cmatch $String){$match = $true}
+        }
+        else
+        {
+            if($thing -match $String){$match = $true}
+        }
+    }
+
+    return $match
+}
